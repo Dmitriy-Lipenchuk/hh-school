e0faa51616ca7378b86c6eef57b4c2f73b484946
Dmitriy Lipenchuk <dmitriy.lipenchuk.01@inbox.ru>
2023-02-02 21:08:18 +0300

    Done

--- parallelism/src/main/java/ru/hh/school/homework/Launcher.java
+++ parallelism/src/main/java/ru/hh/school/homework/Launcher.java
@@ -3,11 +3,17 @@ package ru.hh.school.homework;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 import static java.util.Collections.reverseOrder;
 import static java.util.Map.Entry.comparingByValue;
 import static java.util.function.Function.identity;
@@ -17,7 +23,7 @@ import static java.util.stream.Collectors.toMap;
 
 public class Launcher {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
     // Написать код, который, как можно более параллельно:
     // - по заданному пути найдет все "*.java" файлы
     // - для каждого файла вычислит 10 самых популярных слов (см. #naiveCount())
@@ -37,47 +43,80 @@ public class Launcher {
     // Порядок результатов в консоли не обязательный.
     // При желании naiveSearch и naiveCount можно оптимизировать.
 
-    // test our naive methods:
     testCount();
-    testSearch();
   }
 
-  private static void testCount() {
-    Path path = Path.of("d:\\projects\\work\\hh-school\\parallelism\\src\\main\\java\\ru\\hh\\school\\parallelism\\Runner.java");
-    System.out.println(naiveCount(path));
-  }
+  private static void testCount() throws IOException, InterruptedException {
+    Path path = Path.of(System.getProperty("user.dir"), "\\parallelism\\src\\main\\java\\ru\\hh\\school\\parallelism\\");
+    ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    List<Path> directories = Files.list(path)
+            .filter(Files::isDirectory)
+            .toList();
 
-  private static Map<String, Long> naiveCount(Path path) {
-    try {
-      return Files.lines(path)
-        .flatMap(line -> Stream.of(line.split("[^a-zA-Z0-9]")))
-        .filter(word -> word.length() > 3)
-        .collect(groupingBy(identity(), counting()))
-        .entrySet()
-        .stream()
-        .sorted(comparingByValue(reverseOrder()))
-        .limit(10)
-        .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+    for (Path directory : directories) {
+      CompletableFuture.supplyAsync(() -> getTopTenWordsInDirectory(directory), executor)
+              .thenAccept(map -> {
+                Set<String> words = map.keySet();
+                for (String word : words) {
+                  CompletableFuture.supplyAsync(() -> naiveSearch(word), executor)
+                          .thenAccept(searchResult ->
+                                  System.out.printf("%-100s | %-20s | %-10s\n", directory, word, searchResult));
+                }
+              });
     }
-    catch (IOException e) {
+
+    executor.awaitTermination(1, TimeUnit.SECONDS);
+    executor.shutdown();
+  }
+
+  private static Map<String, Long> getTopTenWordsInDirectory(Path directoryPath) {
+    final Map<String, Long> wordsFrequency = new HashMap<>();
+
+    try {
+      Files.list(directoryPath)
+              .filter(Files::isRegularFile)
+              .forEach(file -> fillMap(file, wordsFrequency));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return wordsFrequency.entrySet()
+            .stream()
+            .sorted(comparingByValue(reverseOrder()))
+            .limit(10)
+            .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+  }
+
+  private static void fillMap(Path filePath, Map<String, Long> wordsFrequency) {
+    try {
+      Files.lines(filePath)
+              .flatMap(line -> Stream.of(line.split("[^a-zA-Z0-9]")))
+              .filter(word -> word.length() > 3)
+              .collect(groupingBy(identity(), counting()))
+              .entrySet()
+              .stream()
+              .sorted(comparingByValue(reverseOrder()))
+              .limit(10)
+              .forEach(x -> wordsFrequency.merge(x.getKey(), x.getValue(), Long::sum));
+    } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private static void testSearch() throws IOException {
-    System.out.println(naiveSearch("public"));
-  }
+  private static long naiveSearch(String query) {
+    Document document = null;
+    try {
+      document = Jsoup //
+              .connect("https://www.google.com/search?q=" + query) //
+              .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36") //
+              .get();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
 
-  private static long naiveSearch(String query) throws IOException {
-    Document document = Jsoup //
-      .connect("https://www.google.com/search?q=" + query) //
-      .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36") //
-      .get();
-
-    Element divResultStats = document.select("div#slim_appbar").first();
+    Element divResultStats = document.select("div#result-stats").first();
     String text = divResultStats.text();
     String resultsPart = text.substring(0, text.indexOf('('));
     return Long.parseLong(resultsPart.replaceAll("[^0-9]", ""));
   }
-
 }
